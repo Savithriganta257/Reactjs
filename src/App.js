0d1a935d0fef7bb677e3567dd238a2f06f066101
Savithriganta257 <savithriganta2003@gmail.com>
2022-11-30 12:01:50 +0530

    about component and props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+/*import ClassComponent from  "./ClassComponent"
+import ClassComponent2 from "./ClassComponent2"
+function App(){
+  return(
+    <div>
+      <p>Good morning</p>
+      <h1>Good day</h1>
+      <h2>Fun day</h2>
+     <img src="https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/07/insert-image-html.jpg" alt=""/>
+      <ClassComponent/>
+      <ClassComponent2/>
     </div>
-  );
+    )
 }
-
-export default App;
+export default App*/
+import PropsExample from "./PropsExample"
+function  App(){
+   return(
+      <div>
+        <h1>This is about props  example</h1>
+        <PropsExample  name="savi" age="20"/>
+      </div>
+      )
+}
+export default App
